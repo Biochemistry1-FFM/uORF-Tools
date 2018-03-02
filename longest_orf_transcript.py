40a3213a199f6c38eb3aaa13b5028734b09e2264
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-03-02 21:46:05 +0100

    Added script to extract longest orf transcripts

---
+++ longest_orf_transcript.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import sys
+import re
+
+
+def gtf_gene_id(gtf):
+    r"""Get gene id from GTF line
+    >>> gtf_gene_id('chr1\tENSEMBL\tUTR\t70006\t70108\t.\t+\t.\tgene_id \"ENSG00000186092.5\"; transcript_id "ENST00000335137.4"; gene_type "protein_coding"; gene_name "OR4F5"; transcript_type "protein_coding"; transcript_name "OR4F5-201"; exon_number 1; exon_id "ENSE00002319515.2"; level 3; protein_id "ENSP00000334393.3"; transcript_support_level "NA"; tag "basic"; tag "appris_principal_1"; tag "CCDS"; ccdsid "CCDS30547.1"; havana_gene "OTTHUMG00000001094.3";')
+    'ENSG00000186092.5'
+    """
+    gtffields = gtf.split("\t")
+    #raise RuntimeError("*\n"+ str(len(gtffields))  + "*\n")
+    extravalues = gtffields[8].split(";")
+    gene_id = extravalues[0]
+    line_gene = re.findall(r'gene_id', gene_id)
+    if line_gene:
+        dkey = re.sub('gene_id "', "", gene_id)
+        dkey2 = re.sub('"', "", dkey)
+        return dkey2
+    else:
+        return None
+
+
+def gtf_transkript_length(orf):
+    r"""Get transkript length from GTF
+    >>> gtf_transkript_length("chr1\tENSEMBL\tUTR\t70006\t70108\t.\t+\t.\tgene_id \"ENSG00000186092.5\"; transcript_id \"ENST00000335137.4\"; gene_type \"protein_coding\"; gene_name \"OR4F5\"; transcript_type \"protein_coding\"; transcript_name \"OR4F5-201\"; exon_number 1; exon_id \"ENSE00002319515.2\"; level 3; protein_id \"ENSP00000334393.3\"; transcript_support_level \"NA\"; tag \"basic\"; tag \"appris_principal_1\"; tag \"CCDS\"; ccdsid \"CCDS30547.1\"; havana_gene \"OTTHUMG00000001094.3\";")
+    102
+    >>> gtf_transkript_length("chr1\tENSEMBL\tUTR\t70108\t70006\t.\t-\t.\tgene_id \"ENSG00000186092.5\"; transcript_id \"ENST00000335137.4\"; gene_type \"protein_coding\"; gene_name \"OR4F5\"; transcript_type \"protein_coding\"; transcript_name \"OR4F5-201\"; exon_number 1; exon_id \"ENSE00002319515.2\"; level 3; protein_id \"ENSP00000334393.3\"; transcript_support_level \"NA\"; tag \"basic\"; tag \"appris_principal_1\"; tag \"CCDS\"; ccdsid \"CCDS30547.1\"; havana_gene \"OTTHUMG00000001094.3\";")
+    102
+    """
+    gtffields = orf.split("\t")
+    strand = gtffields[6]
+    [strand] = re.findall(r'^[-+]$', strand)
+    start = gtffields[3]
+    end = gtffields[4]
+    if strand == "+":
+        length = int(end) - int(start)
+        return length
+    else:
+        rlength = int(start) - int(end)
+        return rlength
+
+
+def main():
+    input_file = ((sys.argv)[1])
+    orfs = []
+    with open(input_file) as origin_file:
+        for line in origin_file:
+            lineCoding = re.findall(r'protein_coding', line)
+            if lineCoding:
+                gtffields = line.split("\t")
+                if gtffields[2] == "transcript":
+                    orfs.append(line)
+    transcripts = {}
+    for orf in orfs:
+        dkey = gtf_gene_id(orf)
+        if dkey is not None:
+            if dkey in transcripts:
+                transcripts[dkey].extend([orf])
+            else:
+                transcripts[dkey] = [orf]
+
+    for key, value in transcripts.items():
+        max_orf = ""
+        max_length = 0
+        for orf in value:
+            orf_length = gtf_transkript_length(orf)
+            if orf_length > max_length:
+                max_orf = orf
+                max_length = orf_length
+        if max_orf:
+            print(max_orf, end='')
+
+
+if __name__ == '__main__':
+    main()
